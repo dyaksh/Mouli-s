a841d44ba43150e59183f2de6dc27c85ce12c86d
Yaksh Darji <95200752+dyaksh@users.noreply.github.com>
2024-08-02 17:21:59 +0530

    proper update

---
+++ aboutus.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us | Moulis Advertising</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <?php include "includes/header.php"; ?>
+
+    <!-- About Us Section -->
+    <section id="aboutus" class="aboutus section">
+        <div class="container">
+            <!-- Section Title -->
+            <div class="section-title" data-aos="fade-up">
+                <h2>About Us</h2>
+            </div><!-- End Section Title -->
+
+            <div class="row gy-4">
+                <div class="col-lg-6 d-flex flex-column justify-content-center">
+                    <h3>Arvind H Mittal</h3>
+                    <h4>Managing Director & CEO</h4>
+                    <blockquote>
+                        "The ultimate objective of our advertising solutions is to provide clients the best returns on their investments by enhancing their brand visibility. Furthermore, the strategic plans accompanied by comprehensive research guide the creative solutions for our government and public sector clients to ensure persuasive and conclusive communication, bearing a perfect fit with their mandate. Simply put, with clarity of purpose, we help reach defined target audiences with the right message and frequency to make businesses profitable."
+                    </blockquote>
+                </div>
+                <div class="col-lg-6 aboutus-img">
+                    <img src="assets/img/aboutus/arvind-sir.png" class="img-fluid" alt="Arvind H Mittal" style="width:50%;">
+                </div>
+            </div>
+            
+
+        </div><!-- End Container -->
+    </section><!-- /About Us Section -->
+
+    <?php include 'includes/footer.php'; ?>
+
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
